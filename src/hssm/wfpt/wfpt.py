21b9f306adfa88561290d35e42e5ac5528564a58
Paul Xu <yang_xu@brown.edu>
2023-01-26 16:54:10 -0500

    dynamically create RandomVariable, added maker function for families

--- src/hssm/wfpt/wfpt.py
+++ src/hssm/wfpt/wfpt.py
@@ -7,8 +7,9 @@ generation ops.
 from __future__ import annotations
 
 from os import PathLike
-from typing import Callable, List, Tuple, Type
+from typing import Callable, Dict, List, Tuple, Type
 
+import bambi as bmb
 import numpy as np
 import onnx
 import pymc as pm
@@ -24,37 +25,74 @@ from .lan import make_jax_logp_funcs_from_onnx, make_jax_logp_ops, make_pytensor
 LogLikeFunc = Callable[..., ArrayLike]
 LogLikeGrad = Callable[..., ArrayLike]
 
-# pylint: disable=W0511, R0903
-# This is just a placeholder to get the code to run at the moment
-class WFPTRandomVariable(RandomVariable):
-    """WFPT random variable"""
 
-    name: str = "WFPT_RV"
-    ndim_supp: int = 0
-    ndims_params: List[int] = [0] * 10
-    dtype: str = "floatX"
-    _print_name: Tuple[str, str] = ("WFPT", "\\operatorname{WFPT}")
+def make_rv(list_params: List[str]) -> Type[RandomVariable]:
+    """Builds a RandomVariable Op according to the list of parameters.
 
-    @classmethod
-    # pylint: disable=arguments-renamed,bad-option-value,W0221
-    def rng_fn(  # type: ignore
-        cls,
-        list_params: List[str] | None,
-        model: str = "ddm",
-        size: int = 500,
-        **kwargs,
-    ) -> np.ndarray:
-        """Generates random variables from this distribution."""
-        sim_out = simulator(theta=list_params, model=model, n_samples=size, **kwargs)
-        data_tmp = sim_out["rts"] * sim_out["choices"]
-        return data_tmp.flatten()
+    Args:
+        list_params (List[str]): a list of str of all parameters for this RandomVariable
+
+    Returns:
+        Type[RandomVariable]: a class of RandomVariable that are to be used in
+            a pm.Distribution.
+    """
+
+    # pylint: disable=W0511, R0903
+    class WFPTRandomVariable(RandomVariable):
+        """WFPT random variable"""
+
+        name: str = "WFPT_RV"
+
+        # NOTE: This is wrong at the moment, but necessary
+        # to get around the support checking in PyMC that would result in error
+        ndim_supp: int = 0
+
+        ndims_params: List[int] = [0 for _ in list_params]
+        dtype: str = "floatX"
+        _print_name: Tuple[str, str] = ("WFPT", "\\operatorname{WFPT}")
+        _list_params = list_params
+
+        # pylint: disable=arguments-renamed,bad-option-value,W0221
+        def rng_fn(  # type: ignore
+            self,
+            rng: np.random.Generator,
+            *args,
+            model: str = "ddm",
+            size: int = 500,
+            **kwargs,
+        ) -> np.ndarray:
+            """Generates random variables from this distribution."""
+            iinfo32 = np.iinfo(np.int32)
+            seed = rng.integers(iinfo32.min, iinfo32.max, dtype=np.int32)
+
+            # Uses a "theta" in `kwargs` to specify how `theta` is passed to
+            # the simulator object
+            if "theta" in kwargs:
+                dict_params = dict(zip(self._list_params, args))
+                theta = [dict_params[param] for param in kwargs["theta"]]
+            else:
+                theta = list(args)
+
+            # Because the `theta` parameter requires
+
+            sim_out = simulator(
+                theta=theta, model=model, n_samples=size, random_state=seed, **kwargs
+            )
+            data_tmp = np.column_stack([sim_out["rts"], sim_out["choices"]])
+            return data_tmp
+
+    return WFPTRandomVariable
 
 
 def make_distribution(
     loglik: LogLikeFunc | pytensor.graph.Op | None,
     rv: Type[RandomVariable] | None,
-    list_params: List[str] | None,
+    list_params: List[str],
 ) -> Type[pm.Distribution]:
+
+    if rv is None:
+        rv = make_rv(list_params)
+
     class WFPTDistribution(pm.Distribution):
         """Wiener first-passage time (WFPT) log-likelihood for LANs."""
 
@@ -63,7 +101,7 @@ def make_distribution(
 
         # NOTE: replace this default when we have a better random number generation
         # method. This is here as a place holder.
-        rv_op = rv() if rv is not None else WFPTRandomVariable()
+        rv_op = rv
         params = list_params
 
         @classmethod
@@ -120,3 +158,35 @@ def make_ssm_distribution(
         return make_distribution(lan_logp_jax, rv, list_params)
 
     raise ValueError("Currently only 'pytensor' and 'jax' backends are supported.")
+
+
+def make_family(
+    dist: pm.Distribution,
+    list_params: List[str],
+    link: str | Dict[str, bmb.families.Link],
+    parent: str = "v",
+    likelihood_name: str = "WFPT Likelihood",
+    family_name="WFPT Family",
+) -> bmb.Family:
+    """Builds a family in bambi.
+
+    Args:
+        dist (pm.Distribution): a pm.Distribution class (not an instance).
+        list_params (List[str]): a list of parameters for the likelihood function.
+        link (str | Dict[str, bmb.families.Link]): a link function or a dictionary of
+            parameter: link functions.
+        parent (str): the parent parameter of the likelihood function. Defaults to v.
+        likelihood_name (str): the name of the likelihood function.
+        family_name (str): the name of the family.
+
+    Returns:
+        bmb.Family: an instance of a bambi family.
+    """
+
+    likelihood = bmb.Likelihood(
+        likelihood_name, parent=parent, params=list_params, dist=dist
+    )
+
+    family = bmb.Family(family_name, likelihood=likelihood, link=link)
+
+    return family
