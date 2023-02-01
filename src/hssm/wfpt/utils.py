8d6d2dc54b5ee79145b49eb2b5b043db1a4936cb
aomar7 <aisuluomar123@gmail.com>
2023-02-01 11:22:28 -0500

    fix: updating type

--- src/hssm/wfpt/utils.py
+++ src/hssm/wfpt/utils.py
@@ -8,7 +8,7 @@ def data_check(
     data: pd.DataFrame,
     response_rates: str = None,
     response: str = None,
-    additional_args: List[str] | None = None,
+    additional_args: List[Optional[str]] | None = None,
 ) -> pd.DataFrame:
     """
     Convert data into correct format before passing it to the hssm models
