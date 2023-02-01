79e9dec57c1906e05478cd824f0f99cac9746258
aomar7 <aisuluomar123@gmail.com>
2023-02-01 11:09:23 -0500

    updating simple wrapper

--- src/hssm/wfpt/simple_wrapper.py
+++ src/hssm/wfpt/simple_wrapper.py
@@ -5,14 +5,12 @@ import pymc as pm
 import pytensor
 
 from hssm import wfpt
+from hssm.wfpt.utils import data_check
 
-from .utils import data_check
 
-
-def create_model(
+def create_hssm(
     data: pd.DataFrame,
     model: str = "base",
-    list_params: List[str] = ["v", "sv", "a", "z", "t"],
     cores: int = 2,
     draws: int = 500,
     tune: int = 500,
@@ -22,28 +20,52 @@ def create_model(
     additional_args: List[str] = None,
 ):
     pytensor.config.floatX = "float32"
+    parameters = {
+        "base": {
+            "model": "base",
+            "list_params": ["v", "sv", "a", "z", "t"],
+            "backend": "pytensor",
+        },
+        "lan": {
+            "model": "test.onnx",
+            "list_params": ["v", "sv", "a", "z", "theta"],
+            "backend": "jax",
+        },
+    }
+    params = parameters[model]["list_params"]
     model = wfpt.make_ssm_distribution(
-        model=model,
-        list_params=list_params,
+        model=parameters[model]["model"],
+        list_params=parameters[model]["list_params"],
+        backend=parameters[model]["backend"],
     )
     data = data_check(data, response_rates, response, additional_args)
     with pm.Model() as m_angle:
-        v = pm.Uniform("v", -3.0, 3.0)
-        sv = pm.Uniform("sv", 0.0, 1.2)
-        a = pm.Uniform("a", 2.0, 3.5)
-        z = pm.Uniform("z", 0.1, 0.9)
-        t = pm.Uniform("t", 0.0, 1.0)
+        param0 = pm.Uniform(params[0], -3.0, 3.0)
+        param1 = pm.Uniform(params[1], 0.0, 1.2)
+        param2 = pm.Uniform(params[2], 2.0, 3.5)
+        param3 = pm.Uniform(params[3], 0.1, 0.9)
+        param4 = pm.Uniform(params[4], 0.0, 1.0)
+        if "t" in params:
+            rt = model(
+                name="rt",
+                v=param0,
+                sv=param1,
+                a=param2,
+                z=param3,
+                t=param4,
+                observed=data,
+            )
+            samples = pm.sample(cores=cores, draws=draws, tune=tune, mp_ctx=mp_ctx)
+        else:
+            rt = model(
+                name="rt",
+                v=param0,
+                sv=param1,
+                a=param2,
+                z=param3,
+                theta=param4,
+                observed=data,
+            )
+            samples = pm.sample(cores=cores, draws=draws, tune=tune, mp_ctx=mp_ctx)
 
-        rt = model(
-            name="rt",
-            v=v,
-            sv=sv,
-            a=a,
-            z=z,
-            t=t,
-            observed=data,
-        )
-        trace_ddm_nuts_jax_wrapped = pm.sample(
-            cores=cores, draws=draws, tune=tune, mp_ctx=mp_ctx
-        )
-    return trace_ddm_nuts_jax_wrapped
+    return samples
