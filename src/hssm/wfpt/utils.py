00c85f80bf98db25c9ea41e2b8eb3d28a5a9b95d
aomar7 <aisuluomar123@gmail.com>
2023-02-02 10:05:57 -0500

    fix: updating data_check function

--- src/hssm/wfpt/utils.py
+++ src/hssm/wfpt/utils.py
@@ -8,7 +8,7 @@ def data_check(
     data: pd.DataFrame,
     response_rates: str = None,
     response: str = None,
-    additional_args: List = None,
+    additional_args: List[str] = None,
 ) -> pd.DataFrame:
     """
     Convert data into correct format before passing it to the hssm models
@@ -18,14 +18,13 @@ def data_check(
     response: name of the column indicating response rates
     additional_args: list of additional columns that will be used in the model
     """
-    if additional_args is None: additional_args = []
-    replace_dict = {
-        ("rt", "response", None): ["rt", "response"],
-        (None, None, None): ["rt", "response"],
-        (None, None, *additional_args): ["rt", "response", *additional_args],
-        (response_rates, response, *additional_args): [response_rates, response, *additional_args],
-        (response_rates, response, None): [response_rates, response],
-    }
-    new_columns = replace_dict[tuple([response_rates, response, *additional_args])]
+    if additional_args is None:
+        additional_args = []
+    if response_rates is None:
+        response_rates = "rt"
+    if response is None:
+        response = "response"
+
+    new_columns = [response_rates, response, *additional_args]
     data = data[new_columns]
     return data
