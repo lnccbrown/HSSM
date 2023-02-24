5fa43438f3a2a01d4494e99ddc9b90267295f34b
aomar7 <aisuluomar123@gmail.com>
2023-02-24 15:22:30 -0500

    feat: cleaning code, adding doctstrings

--- src/hssm/hssm.py
+++ src/hssm/hssm.py
@@ -13,14 +13,33 @@ from hssm.wfpt.config import default_model_config
 # add custom link function
 class HSSM:
     """
-    Initialize the HSSM class.
+    The Hierarchical Sequential Sampling Model (HSSM) class.
 
     Args:
-        data: A pandas DataFrame containing the data to be analyzed.
-        model_name: The name of the model to use. Default is "analytical".
-        include: A list of dictionaries specifying additional parameters
-         to include in the model.
-        model_config: A dictionary containing the model configuration information.
+    data (pandas.DataFrame): A pandas DataFrame containing the data to be
+     analyzed.
+    model_name (str): The name of the model to use. Default is "analytical".
+    include (List[dict], optional): A list of dictionaries specifying additional
+     parameters to include in the model. Defaults to None.
+    model_config (dict, optional): A dictionary containing the model
+     configuration information. Defaults to None.
+
+    Attributes:
+        list_params (list): The list of parameter names.
+        model_name (str): The name of the model.
+        ssm_model: A SSM model object.
+        likelihood: A Bambi likelihood object.
+        family: A Bambi family object.
+        priors (dict): A dictionary containing the prior distribution
+         of parameters.
+        formula (str): A string representing the model formula.
+        params (list): A list of Param objects representing model parameters.
+        model: A Bambi model object.
+
+    Methods:
+        _transform_include: A method to transform the include list
+         into Bambi's format.
+        sample: A method to sample posterior distributions.
     """
 
     def __init__(
@@ -34,7 +53,8 @@ class HSSM:
             model_config if model_config is not None else default_model_config
         )
         self.list_params = self.model_config[model_name]["list_params"]
-        if model_name == "analytical":
+        self.model_name = model_name
+        if self.model_name == "analytical":
             self.ssm_model = wfpt.WFPT
         else:
             self.ssm_model = wfpt.make_ssm_distribution(
@@ -56,10 +76,26 @@ class HSSM:
         )
         self.priors = {}
         for param in self.list_params:
-            self.priors[param] = bmb.Prior(  # type: ignore
-                self.model_config[model_name]["prior"][param]["name"],  # type: ignore
-                lower=self.model_config[model_name]["prior"][param]["lower"],
-                upper=self.model_config[model_name]["prior"][param]["upper"],
+            self.priors[param] = (
+                bmb.Prior(
+                    self.model_config[model_name]["prior"][param]["name"],
+                    lower=self.model_config[model_name]["prior"][param]["lower"],
+                    upper=self.model_config[model_name]["prior"][param]["upper"],
+                )
+                if param != "v"
+                else {
+                    "Intercept": bmb.Prior(
+                        self.model_config[model_name]["prior"][param]["Intercept"][
+                            "name"
+                        ],
+                        lower=self.model_config[model_name]["prior"][param][
+                            "Intercept"
+                        ]["lower"],
+                        upper=self.model_config[model_name]["prior"][param][
+                            "Intercept"
+                        ]["upper"],
+                    )
+                }
             )
 
         self.formula = self.model_config[model_name]["formula"]
@@ -71,13 +107,18 @@ class HSSM:
             self.formula, data, family=self.family, priors=self.priors
         )
 
-    def _transform_include(self, params: List[dict]) -> None:
-        formulas = [p["formula"] for p in params if p.get("formula")]
-        first_item = formulas[0].split(" ~ ")[0]
-        formulas[0] = formulas[0].replace(first_item, "c(rt,response)")
+    def _transform_include(self, include: List[dict]) -> None:
+        formulas = [p.get("formula") for p in include]
+        v_dict = next((p for p in include if p.get("name") != "v"), None)
+        if v_dict:
+            formulas.insert(0, self.model_config[self.model_name]["formula"])
+        else:
+            first_item = formulas[0].split(" ~ ")[0]
+            formulas[0] = formulas[0].replace(first_item, "c(rt,response)")
+
         self.formula = bmb.Formula(*formulas)
         self.params = []
-        for dictionary in params:
+        for dictionary in include:
             self.params.append(Param(**dictionary))
             coefs = dictionary["formula"].split(" ~ ")[1]
             coefs = coefs.split(" + ")
@@ -107,6 +148,23 @@ class HSSM:
         mp_ctx: str = "fork",
         sampler: str = "pytensor",
     ):
+        """
+        Perform posterior sampling using the `fit` function of Bambi.
+
+        Args:
+            cores (int): The number of cores to use for parallel sampling.
+             Default is 2.
+            draws (int): The number of posterior samples to draw. Default is 500.
+            tune (int): The number of tuning steps to perform before starting
+             to draw posterior samples. Default is 500.
+            mp_ctx (str): The multiprocessing context to use. Default is "fork".
+            sampler (str): The sampler to use. Can be either "jax" or "pytensor".
+             Default is "pytensor".
+
+        Returns:
+            The posterior samples, which is an instance of the
+             `arviz.InferenceData` class.
+        """
         if sampler == "jax":
             return self.model.fit(
                 cores=cores,
