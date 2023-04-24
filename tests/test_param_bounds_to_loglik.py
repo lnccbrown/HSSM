cf5a85a49cc9416f80952df145222e5c8ea5f6b9
aomar7 <aisuluomar123@gmail.com>
2023-04-24 18:53:11 -0400

    feat: addressing PR review comments

--- tests/test_param_bounds_to_loglik.py
+++ tests/test_param_bounds_to_loglik.py
@@ -53,8 +53,10 @@ def test_adjust_logp_with_analytical(data, a):
         err,
         default_boundaries=default_model_config["ddm"]["default_boundaries"],
     )
-    assert pt.all(pt.eq(adjusted_logp, logp)).eval()
-    assert pt.all(pt.eq(adjusted_logp, pt.full_like(adjusted_logp, -66.1))).eval()
+    assert pt.all(pt.eq(adjusted_logp, logp)).eval() == True
+    assert (
+        pt.all(pt.eq(adjusted_logp, pt.full_like(adjusted_logp, -66.1))).eval() == False
+    )
 
 
 @pytest.mark.parametrize("theta", [0.5, -4.0])
@@ -77,5 +79,7 @@ def test_adjust_logp_with_angle(data_angle, fixture_path, theta):
         theta,
         default_boundaries=default_model_config["angle"]["default_boundaries"],
     )
-    assert pt.all(pt.eq(adjusted_logp, logp_angle)).eval()
-    assert pt.all(pt.eq(adjusted_logp, pt.full_like(adjusted_logp, -66.1))).eval()
+    assert pt.all(pt.eq(adjusted_logp, logp_angle)).eval() == True
+    assert (
+        pt.all(pt.eq(adjusted_logp, pt.full_like(adjusted_logp, -66.1))).eval() == False
+    )
