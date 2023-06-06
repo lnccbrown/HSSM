52323110ba9285a4bbf14424d6ca8f53ef57b7b3
aomar7 <aisuluomar123@gmail.com>
2023-06-06 09:14:57 -0700

    feat: updating tests to reflect the new update

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,6 +4,9 @@ from pathlib import Path
 
 def test_model_paths():
     """Ensures all default model onnx files exist in the correct path."""
-    for _, config in default_model_config.items():
+    for model, config in default_model_config.items():
         if config["loglik_kind"] == "approx_differentiable":
-            assert Path(config["loglik_path"]).exists()
+            if model == 'custom_angle':
+                assert config["loglik_path"] is None, "The `loglik_path` should be None for `custom_angle` model."
+            else:
+                assert Path(config["loglik_path"]).exists()
