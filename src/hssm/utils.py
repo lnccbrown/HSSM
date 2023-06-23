7e153d92b12de0456c8f1ac255536c37846abbbe
aomar7 <aisuluomar123@gmail.com>
2023-06-23 13:03:01 -0400

    feat: fixing update

--- src/hssm/utils.py
+++ src/hssm/utils.py
@@ -29,33 +29,8 @@ from huggingface_hub import hf_hub_download
 # and reusable
 ParamSpec = Union[float, dict[str, Any], bmb.Prior]
 BoundsSpec = tuple[float, float]
+
 REPO_ID = "franklab/HSSM"
-
-
-def download_hf(path: str):
-    """
-    Download a file from a HuggingFace repository.
-
-    Parameters
-    ----------
-    path : str
-        The path of the file to download in the repository.
-
-    Returns
-    -------
-    str
-        The local path where the file is downloaded.
-
-    Notes
-    -----
-    The repository is specified by the REPO_ID constant,
-    which should be a valid HuggingFace.co repository ID.
-    The file is downloaded using the HuggingFace Hub's
-     hf_hub_download function.
-    """
-    return hf_hub_download(repo_id=REPO_ID, filename=path)
-
-REPO_ID = "Aisulu/hssm_onnx_models"
 
 
 def download_hf(path: str):
