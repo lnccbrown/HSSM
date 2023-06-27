0e0c0fb27606d7b73496f387f7b1b9aecc66221c
Paul Xu <yang_xu@brown.edu>
2023-06-27 13:00:13 -0400

    expose download_hf

--- src/hssm/distribution_utils/__init__.py
+++ src/hssm/distribution_utils/__init__.py
@@ -8,8 +8,10 @@ from .dist import (
     make_family,
     make_ssm_rv,
 )
+from ..utils import download_hf
 
 __all__ = [
+    "download_hf",
     "make_blackbox_op",
     "make_distribution",
     "make_distribution_from_blackbox",
