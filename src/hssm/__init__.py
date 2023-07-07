fa6bfbbccbe7cfb60fbf32f0642d908c74b8517d
Paul Xu <yang_xu@brown.edu>
2023-07-06 20:32:16 -0400

    rebase to main

--- src/hssm/__init__.py
+++ src/hssm/__init__.py
@@ -3,5 +3,11 @@
 from .datasets import load_data
 from .hssm import HSSM
 from .simulator import simulate_data
+from .utils import set_floatX
 
-__all__ = ["HSSM", "load_data", "simulate_data"]
+__all__ = [
+    "HSSM",
+    "load_data",
+    "simulate_data",
+    "set_floatX",
+]
