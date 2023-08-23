cb5d7fb0b494c3de54d740eab89475a3b456dcb0
Paul Xu <yang_xu@brown.edu>
2023-08-23 10:50:08 -0400

    Add blackbox likelihood functions to __init__.py

--- src/hssm/likelihoods/__init__.py
+++ src/hssm/likelihoods/__init__.py
@@ -1,5 +1,13 @@
 """Likelihood functions and distributions that use them."""
 
 from .analytical import DDM, DDM_SDV, logp_ddm, logp_ddm_sdv
+from .blackbox import logp_ddm_bbox, logp_ddm_sdv_bbox
 
-__all__ = ["logp_ddm", "logp_ddm_sdv", "DDM", "DDM_SDV"]
+__all__ = [
+    "logp_ddm",
+    "logp_ddm_sdv",
+    "DDM",
+    "DDM_SDV",
+    "logp_ddm_bbox",
+    "logp_ddm_sdv_bbox",
+]
