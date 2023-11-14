06b0567dee32486e2e4397c6e2ccdcbd210573d5
Paul Xu <yang_xu@brown.edu>
2023-11-14 13:49:37 -0500

    export plot_quantile_probability

--- src/hssm/plotting/__init__.py
+++ src/hssm/plotting/__init__.py
@@ -1,5 +1,6 @@
 """Plotting functionalities for HSSM."""
 
 from .posterior_predictive import plot_posterior_predictive
+from .quantile_probability import plot_quantile_probability
 
-__all__ = ["plot_posterior_predictive"]
+__all__ = ["plot_posterior_predictive", "plot_quantile_probability"]
