64e356e2e60820e6573dfee1e71b401f11d00e90
Fengler <alexanderfengler@gmx.de>
2024-05-06 16:34:25 +0200

    fix sample function to not create naming conflict for v

--- src/hssm/hssm.py
+++ src/hssm/hssm.py
@@ -500,11 +500,10 @@ class HSSM:
         # 'v' was previously not part of deterministics --> compute it via
         # posterior_predictive (works because it acts as the 'mu' parameter
         # in the GLM as far as bambi is concerned)
-        self.sample_posterior_predictive(self._inference_obj, kind="mean")
-
-        # rename 'rt,response_mean' to 'v' so in the traces everything
-        # looks the way it should
-        if self._inference_obj is not None:
+        if "v" not in self._inference_obj.posterior.data_vars.keys():
+            self.sample_posterior_predictive(self._inference_obj, kind="mean")
+            # rename 'rt,response_mean' to 'v' so in the traces everything
+            # looks the way it should
             self._inference_obj.rename_vars({"rt,response_mean": "v"}, inplace=True)
         return self.traces
 
