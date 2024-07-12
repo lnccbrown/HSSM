dcc208c55d4fc89b9bc8a89382ee967f6141e811
Paul Xu <yang_xu@brown.edu>
2024-07-12 08:45:53 -0400

    update docstring to clarify behavior

--- src/hssm/hssm.py
+++ src/hssm/hssm.py
@@ -460,8 +460,9 @@ class HSSM:
         Returns
         -------
         az.InferenceData | pm.Approximation
-            A reference to the `model.traces` object, which is an ArviZ `InferenceData`
-            instance if inference_method is `"mcmc"` (default), `"nuts_numpyro"`,
+            A reference to the `model.traces` object, which stores the traces of the
+            last call to `model.sample()`. `model.traces` is an ArviZ `InferenceData`
+            instance if `sampler` is `"mcmc"` (default), `"nuts_numpyro"`,
             `"nuts_blackjax"` or "`laplace"`, or an `Approximation` object if `"vi"`.
         """
         # If initvals are None (default)
