30fd9f112c6f53c5a2c89f8f6888a45d6081e9cc
Fengler <alexanderfengler@gmx.de>
2024-11-19 20:42:48 -0500

    fix tests that involve priors attribute

--- tests/test_hssm.py
+++ tests/test_hssm.py
@@ -135,15 +135,15 @@ def test_custom_model(data_ddm):
 def test_model_definition_outside_include(data_ddm):
     model_with_one_param_fixed = HSSM(data_ddm, a=0.5)
 
-    assert "a" in model_with_one_param_fixed.priors
-    assert model_with_one_param_fixed.priors["a"] == 0.5
+    assert "a" in model_with_one_param_fixed.params
+    assert model_with_one_param_fixed.params["a"].prior == 0.5
 
     model_with_one_param = HSSM(
         data_ddm, a={"prior": {"name": "Normal", "mu": 0.5, "sigma": 0.1}}
     )
 
-    assert "a" in model_with_one_param.priors
-    assert model_with_one_param.priors["a"].name == "Normal"
+    assert "a" in model_with_one_param.params
+    assert model_with_one_param.params["a"].prior.name == "Normal"
 
     with pytest.raises(
         ValueError, match="Parameter `a` specified in both `include` and `kwargs`."
@@ -317,5 +317,5 @@ def test_prior_settings_basic(cavanagh_test):
     )
 
     assert isinstance(
-        model_2.params[model_2._parent].prior
+        model_2.params[model_2._parent].prior, dict
     ), "Prior assigned to parent is not a dict!"
