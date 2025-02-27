cf01c4c02ed5e0b71772f420570a756ccc5b276b
Carlos Paniagua <cpaniaguam@gmail.com>
2025-02-27 16:40:00 -0500

    refactor: simplify main function by consolidating search functionality and updating argument parsing

--- src/award/main.py
+++ src/award/main.py
@@ -1,25 +1,19 @@
 import argparse
-from .searchdocx import search_files as search_docx, add_docx_subparser
-from .searchpdf import search_files as search_pdf, add_pdf_subparser
 
-def main():
-    parser = argparse.ArgumentParser(description='Search for keywords in PDF and DOCX files')
-    subparsers = parser.add_subparsers(dest='command', help='Commands')
 
-    # Add DOCX search subcommand using the parser from searchdocx.py
-    add_docx_subparser(subparsers)
+from .utils import search_files
 
-    # Add PDF search subcommand using the parser from searchpdf.py
-    add_pdf_subparser(subparsers)
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Search for keywords in PDF, DOC, and DOCX files. Output is a CSV file."
+    )
+    parser.add_argument("--input", "-i", required=True, help="Input directory")
+    parser.add_argument("--keywords", "-k", required=True, help="Keywords file path")
+    parser.add_argument("--output", "-o", required=True, help="Output csv file path")
 
     args = parser.parse_args()
-
-    if args.command == 'docx':
-        search_docx(args.input, args.keywords, args.output)
-    elif args.command == 'pdf':
-        search_pdf(args.input, args.keywords, args.output)
-    else:
-        parser.print_help()
+    search_files(args.input, args.keywords, args.output)
 
 
 if __name__ == "__main__":
