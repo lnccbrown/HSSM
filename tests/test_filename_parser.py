955909e5d09ecc00782119df20d8db274446b1b7
Carlos Paniagua <cpaniaguam@gmail.com>
2025-03-03 14:42:29 -0500

    test: add unit tests for get_document_code_description function

---
+++ tests/test_filename_parser.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import pytest
+
+from award.utils import get_document_code_description
+
+@pytest.mark.parametrize(
+    "path, expected",
+    [
+        ("1234_4568_name.pdf", ("1234_4568", "name")),
+        ("1234_5678.doc", ("1234_5678", None)),
+        ("My file description.docx", (None, "My file description")),
+    ],
+)
+def test_get_document_code_description(path, expected):
+    assert get_document_code_description(Path(path)) == expected
