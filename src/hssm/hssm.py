44eef92b8332b469efa82426949f24264f97ae66
Fengler <alexanderfengler@gmx.de>
2025-03-04 17:46:42 -0500

    adding priors defaults for p-outliers

--- src/hssm/hssm.py
+++ src/hssm/hssm.py
@@ -1354,9 +1354,9 @@ class HSSM:
             Whether to call plt.tight_layout() after plotting. Defaults to True.
         """
         data = data or self.traces
-        assert isinstance(data, az.InferenceData), (
-            "data must be an InferenceData object."
-        )
+        assert isinstance(
+            data, az.InferenceData
+        ), "data must be an InferenceData object."
 
         if not include_deterministic:
             var_names = list(
@@ -1420,9 +1420,9 @@ class HSSM:
             A pandas DataFrame or xarray Dataset containing the summary statistics.
         """
         data = data or self.traces
-        assert isinstance(data, az.InferenceData), (
-            "data must be an InferenceData object."
-        )
+        assert isinstance(
+            data, az.InferenceData
+        ), "data must be an InferenceData object."
 
         if not include_deterministic:
             var_names = list(
@@ -1738,12 +1738,12 @@ class HSSM:
             )
 
         if self.p_outlier is not None:
-            # TODO: Allow regression for self.p_outlier
-            # Need to determine what the output should look like
-            if self.p_outlier.is_regression:
-                raise NotImplementedError(
-                    "Regression for `p_outlier` is not implemented yet."
-                )
+            # # TODO: Allow regression for self.p_outlier
+            # # Need to determine what the output should look like
+            # if self.p_outlier.is_regression:
+            #     raise NotImplementedError(
+            #         "Regression for `p_outlier` is not implemented yet."
+            #     )
             output.append("")
             output.append(f"Lapse probability: {self.p_outlier.prior}")
             output.append(f"Lapse distribution: {self.lapse}")
