c5c0f9b71408691b215563ce46ee965b20429835
Carlos Paniagua <cpaniaguam@gmail.com>
2025-04-07 00:51:05 -0400

    Refactor get_default_model_config to dynamically import model configuration functions

--- src/hssm/modelconfig/__init__.py
+++ src/hssm/modelconfig/__init__.py
@@ -7,7 +7,7 @@ Functions
 ---------
 get_ddm_config() -> DefaultConfig
 
-get_ddm_svd_config() -> DefaultConfig
+get_ddm_sdv_config() -> DefaultConfig
 
 get_full_ddm_config() -> DefaultConfig
 
@@ -24,20 +24,13 @@ get_ornstein_config() -> DefaultConfig
 get_weibull_config() -> DefaultConfig
 
 get_race_no_bias_angle_4_config() -> DefaultConfig
+
+get_default_model_config(model_name: SupportedModels) -> DefaultConfig
 """
 
+import importlib
+
 from .._types import DefaultConfig, SupportedModels
-from .angle_config import get_angle_config
-from .ddm_config import get_ddm_config
-from .ddm_seq2_no_bias_config import get_ddm_seq2_no_bias_config
-from .ddm_svd_config import get_ddm_svd_config
-from .full_ddm_config import get_full_ddm_config
-from .lba2_config import get_lba2_config
-from .lba3_config import get_lba3_config
-from .levy_config import get_levy_config
-from .ornstein_config import get_ornstein_config
-from .race_no_bias_angle_4_config import get_race_no_bias_angle_4_config
-from .weibull_config import get_weibull_config
 
 
 def get_default_model_config(model_name: SupportedModels) -> DefaultConfig:
@@ -58,20 +51,20 @@ def get_default_model_config(model_name: SupportedModels) -> DefaultConfig:
         and likelihood specifications.
 
     """
-    _map = {
-        "ddm": get_ddm_config,
-        "ddm_sdv": get_ddm_svd_config,
-        "full_ddm": get_full_ddm_config,
-        "angle": get_angle_config,
-        "levy": get_levy_config,
-        "ornstein": get_ornstein_config,
-        "weibull": get_weibull_config,
-        "race_no_bias_angle_4": get_race_no_bias_angle_4_config,
-        "ddm_seq2_no_bias": get_ddm_seq2_no_bias_config,
-        "lba3": get_lba3_config,
-        "lba2": get_lba2_config,
-    }
     try:
-        return _map[model_name]()
-    except KeyError:
-        raise ValueError(f"Model {model_name} not currently registered in HSSM.")
+        # Construct the module name and function name based on the model name
+        module_name = f"{model_name}_config"
+        function_name = f"get_{module_name}"
+
+        # Dynamically import the module
+        module = importlib.import_module(f".{module_name}", package="hssm.modelconfig")
+
+        # Retrieve the function from the module
+        function = getattr(module, function_name)
+
+        # Call and return the function
+        return function()
+    except ModuleNotFoundError:
+        raise ValueError(f"Module for model {model_name} not found in HSSM.")
+    except AttributeError:
+        raise ValueError(f"Function {function_name} not found in module {module_name}.")
