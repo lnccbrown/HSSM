e3503a98d4101249f2cd8e71118b9e038198e5b5
Carlos Paniagua <cpaniaguam@gmail.com>
2025-04-14 10:18:59 -0400

    Mark test_make_jax_logp_ops as slow

--- tests/test_onnx.py
+++ tests/test_onnx.py
@@ -101,7 +101,7 @@ def test_make_jax_logp_funcs_from_onnx(fixture_path):
     )
 
 
-# @pytest.mark.slow
+@pytest.mark.slow
 def test_make_jax_logp_ops(fixture_path):
     """Tests whether the logp Op returned from make_jax_logp_ops with different backends
     work the same way.
