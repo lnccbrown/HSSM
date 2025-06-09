9ffc756d925c47fbd78505181d2802bca4ba88a7
Carlos Paniagua <cpaniaguam@gmail.com>
2025-06-09 16:54:55 -0400

    Enhance data validation in DataValidator with comprehensive post-checks for missing, NaN, and invalid response times

--- src/hssm/data_validator.py
+++ src/hssm/data_validator.py
@@ -39,7 +39,7 @@ class DataValidator:
     def check_fields(a, b):
         """Check if all fields in a are in b."""
         missing = set(a) - set(b)
-        if missing: # there are leftover fields
+        if missing:  # there are leftover fields
             raise ValueError(f"Field(s) `{', '.join(missing)}` not found in data.")
 
     def _check_extra_fields(self, data: pd.DataFrame | None = None) -> bool:
@@ -57,3 +57,47 @@ class DataValidator:
         """Check if the data is clean enough for the model."""
         DataValidator.check_fields(self.response, self.data.columns)
         self._check_extra_fields()
+
+    def _post_check_data_sanity(self):
+        """Check if the data is clean enough for the model."""
+        if self.deadline or self.missing_data:
+            if -999.0 not in self.data["rt"].unique():
+                raise ValueError(
+                    "You have no missing data in your dataset, "
+                    + "which is not allowed when `missing_data` or `deadline` is set to"
+                    + " True."
+                )
+            rt_filtered = self.data.rt[self.data.rt != -999.0]
+        else:
+            rt_filtered = self.data.rt
+
+        if np.any(rt_filtered.isna(), axis=None):
+            raise ValueError(
+                "You have NaN response times in your dataset, "
+                + "which is not allowed."
+            )
+
+        if not np.all(rt_filtered >= 0):
+            raise ValueError(
+                "You have negative response times in your dataset, "
+                + "which is not allowed."
+            )
+
+        valid_responses = self.data.loc[self.data["rt"] != -999.0, "response"]
+        unique_responses = valid_responses.unique().astype(int)
+
+        if np.any(~np.isin(unique_responses, self.choices)):
+            invalid_responses = sorted(
+                unique_responses[~np.isin(unique_responses, self.choices)]
+            )
+            raise ValueError(
+                f"Invalid responses found in your dataset: {invalid_responses}"
+            )
+
+        if len(unique_responses) != self.n_choices:
+            missing_responses = sorted(np.setdiff1d(self.choices, unique_responses))
+            _logger.warning(
+                "You set choices to be %s, but %s are missing from your dataset.",
+                self.choices,
+                missing_responses,
+            )
