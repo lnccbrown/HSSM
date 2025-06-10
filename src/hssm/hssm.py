59d04b6c8cb5d8e65db261d934e18c4ad990b675
Carlos Paniagua <cpaniaguam@gmail.com>
2025-06-10 12:20:30 -0400

    Use DataValidator in HSSM class

--- src/hssm/hssm.py
+++ src/hssm/hssm.py
@@ -1957,21 +1957,6 @@ class HSSM(DataValidator):
             ),
         )
 
-    def _update_extra_fields(self, new_data: pd.DataFrame | None = None):
-        """Update the extra fields data in self.model_distribution.
-
-        Parameters
-        ----------
-        new_data
-            A DataFrame containing new data for update.
-        """
-        if not new_data:
-            new_data = self.data
-
-        self.model_distribution.extra_fields = [
-            new_data[field].values for field in self.extra_fields
-        ]
-
     def _get_deterministic_var_names(self, idata) -> list[str]:
         """Filter out the deterministic variables in var_names."""
         var_names = [
@@ -2022,103 +2007,6 @@ class HSSM(DataValidator):
                         idata[group].rename({"rt,response_mean": self._parent}),
                     )
             return idata
-
-    def _handle_missing_data_and_deadline(self):
-        """Handle missing data and deadline."""
-        if not self.missing_data and not self.deadline:
-            # In the case where missing_data is set to False, we need to drop the
-            # cases where rt = na_value
-            if pd.isna(self.missing_data_value):
-                na_dropped = self.data.dropna(subset=["rt"])
-            else:
-                na_dropped = self.data.loc[
-                    self.data["rt"] != self.missing_data_value, :
-                ]
-
-            if len(na_dropped) != len(self.data):
-                _logger.warning(
-                    "`missing_data` is set to False, "
-                    + "but you have missing data in your dataset. "
-                    + "Missing data will be dropped."
-                )
-            self.data = na_dropped
-
-        elif self.missing_data and not self.deadline:
-            # In the case where missing_data is set to True, we need to replace the
-            # missing data with a specified na_value
-
-            # Create a shallow copy to avoid modifying the original dataframe
-            if pd.isna(self.missing_data_value):
-                self.data["rt"] = self.data["rt"].fillna(-999.0)
-            else:
-                self.data["rt"] = self.data["rt"].replace(
-                    self.missing_data_value, -999.0
-                )
-
-        else:  # deadline = True
-            if self.deadline_name not in self.data.columns:
-                raise ValueError(
-                    "You have specified that your data has deadline, but "
-                    + f"`{self.deadline_name}` is not found in your dataset."
-                )
-            else:
-                self.data.loc[:, "rt"] = np.where(
-                    self.data["rt"] < self.data[self.deadline_name],
-                    self.data["rt"],
-                    -999.0,
-                )
-
-    def _pre_check_data_sanity(self):
-        """Check if the data is clean enough for the model."""
-        for field in self.response:
-            if field not in self.data.columns:
-                raise ValueError(f"Field {field} not found in data.")
-
-        self._check_extra_fields()
-
-    def _post_check_data_sanity(self):
-        """Check if the data is clean enough for the model."""
-        if self.deadline or self.missing_data:
-            if -999.0 not in self.data["rt"].unique():
-                raise ValueError(
-                    "You have no missing data in your dataset, "
-                    + "which is not allowed when `missing_data` or `deadline` is set to"
-                    + " True."
-                )
-            rt_filtered = self.data.rt[self.data.rt != -999.0]
-        else:
-            rt_filtered = self.data.rt
-
-        if np.any(rt_filtered.isna(), axis=None):
-            raise ValueError(
-                "You have NaN response times in your dataset, "
-                + "which is not allowed."
-            )
-
-        if not np.all(rt_filtered >= 0):
-            raise ValueError(
-                "You have negative response times in your dataset, "
-                + "which is not allowed."
-            )
-
-        valid_responses = self.data.loc[self.data["rt"] != -999.0, "response"]
-        unique_responses = valid_responses.unique().astype(int)
-
-        if np.any(~np.isin(unique_responses, self.choices)):
-            invalid_responses = sorted(
-                unique_responses[~np.isin(unique_responses, self.choices)]
-            )
-            raise ValueError(
-                f"Invalid responses found in your dataset: {invalid_responses}"
-            )
-
-        if len(unique_responses) != self.n_choices:
-            missing_responses = sorted(np.setdiff1d(self.choices, unique_responses))
-            _logger.warning(
-                "You set choices to be %s, but %s are missing from your dataset.",
-                self.choices,
-                missing_responses,
-            )
 
     def _postprocess_initvals_deterministic(
         self, initval_settings: dict = INITVAL_SETTINGS
