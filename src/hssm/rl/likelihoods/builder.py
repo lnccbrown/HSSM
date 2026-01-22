ebcd5da4e825780dfca51ef03813aa30441f270c
Carlos Paniagua <cpaniaguam@gmail.com>
2026-01-22 11:07:56 -0500

    Move compute_v_trial_wise and compute_v_subject_wise to two_armed_bandit.py

--- src/hssm/rl/likelihoods/builder.py
+++ src/hssm/rl/likelihoods/builder.py
@@ -42,7 +42,6 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 from jax import Array
-from jax.lax import scan
 
 from hssm.distribution_utils.func_utils import make_vjp_func
 from hssm.distribution_utils.jax import make_jax_logp_ops
@@ -132,82 +131,6 @@ class AnnotatedFunction(Protocol):
 
     # Added to satisfy static type checkers
     def __call__(self, *args: Any, **kwargs: Any) -> Any: ...  # noqa: D102
-
-
-# Inner function to compute the drift rate and update q-values for each trial.
-# This function is used with `jax.lax.scan` to process each trial in the RLDM model.
-def compute_v_trial_wise(
-    q_val: jnp.ndarray, inputs: jnp.ndarray
-) -> tuple[jnp.ndarray, jnp.ndarray]:
-    """Compute the drift rate and updates the q-values for each trial.
-
-    This function is used with `jax.lax.scan` to process each trial. It takes the
-    current q-values and the RL parameters (rl_alpha, scaler), action (response),
-    and reward (feedback) for the current trial, computes the drift rate, and
-    updates the q-values. The q_values are updated in each iteration and carried
-    forward to the next one.
-
-    Parameters
-    ----------
-    q_val
-        A length-2 jnp array containing the current q-values for the two alternatives.
-        These values are updated in each iteration and carried forward to the next
-        trial.
-    inputs
-        A 2D jnp array containing the RL parameters (rl_alpha, scaler),
-        action (response), and reward (feedback) for the current trial.
-
-    Returns
-    -------
-    tuple
-        A tuple containing the updated q-values and the computed drift rate (v).
-    """
-    rl_alpha, scaler, action, reward = inputs
-    action = jnp.astype(action, jnp.int32)
-
-    # drift rate on each trial depends on difference in expected rewards for
-    # the two alternatives:
-    # drift rate = (q_up - q_low) * scaler where
-    # the scaler parameter describes the weight to put on the difference in
-    # q-values.
-    computed_v = (q_val[1] - q_val[0]) * scaler
-
-    # compute the reward prediction error
-    delta_RL = reward - q_val[action]
-
-    # update the q-values using the RL learning rule (here, simple TD rule)
-    q_val = q_val.at[action].set(q_val[action] + rl_alpha * delta_RL)
-
-    return q_val, computed_v
-
-
-# This function computes the drift rates (v) for each subject by processing
-# their trials one by one. It uses `jax.lax.scan` to efficiently iterate over
-# the trials and compute the drift rates based on the RL parameters, actions,
-# and rewards for each trial.
-def compute_v_subject_wise(
-    subj_trials: jnp.ndarray,
-) -> jnp.ndarray:
-    """Compute the drift rates (v) for a given subject.
-
-    Parameters
-    ----------
-    subj_trials:
-        A jnp array of dimension (n_trials, 4) containing rl_alpha, scaler,
-        action (response), and reward (feedback) for each trial of the subject.
-
-    Returns
-    -------
-    jnp.ndarray
-        The computed drift rates (v) for the RLDM model for the given subject.
-    """
-    _, v = scan(
-        compute_v_trial_wise,
-        jnp.ones(2) * 0.5,  # initial q-values for the two alternatives
-        subj_trials,
-    )
-
-    return v
 
 
 def _get_column_indices(
